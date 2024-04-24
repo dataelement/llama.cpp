9134b9bcda9eb685b4da5582593a054a8a8586ee
hanfeng <hanfeng@dataelem.com>
2024-04-24 17:18:32 +0800

    update test_perf

--- examples/server/test_data/function_calls/test_perf.py
+++ examples/server/test_data/function_calls/test_perf.py
@@ -4,24 +4,22 @@ import argparse
 import json
 
 
-def batch_call(bodys, api_url, model_name):
+def batch_call(bodys, api_url, model_name, result_file):
     client = OpenAI(
     api_key="dummy",
     base_url=api_url)
 
-    for body in bodys:
-        print(json.dumps(body, ensure_ascii=False, indent=2))
-        print('\n---')
-        print(">>>Q: {}".format(body['messages']))
+    with open(result_file, 'w') as fout:
+        for body in bodys:
+            # print('---\n', json.dumps(body, ensure_ascii=False, indent=2))
+            completion = client.chat.completions.create(
+                model=model_name,
+                messages=body['messages'],
+                tools=body['tools'],
+                temperature=0.3)
 
-        completion = client.chat.completions.create(
-            model=model_name,
-            messages=body['messages'],
-            tools=body['tools'],
-            temperature=0.3)
-
-        print(">>>A: {}".format(completion.choices[0].message.content))
-        print(">>>Tools: {}".format(completion.choices[0].message.tool_calls))
+            result = {'input': body, 'output': completion.choices[0].message.dict()}
+            fout.write(json.dumps(result, ensure_ascii=False) + "\n")
 
 
 def trans_conv_oai_request(conv):
@@ -33,26 +31,52 @@ def trans_conv_oai_request(conv):
         parameters = {}
         parameters['type'] = 'object'
         parameters['properties'] = {}
+        ori_required = tool['parameters'].get('required', [])
+        required = []
         for prop_key, prop_value in tool['parameters']['properties'].items():
-            if prop_value['type'] in ['string', 'number', 'boolean', 'array', 'integer', 'int', 'float']:
+            if prop_value['type'] in ['string', 'number', 'boolean', 'integer', 'int', 'float']:
                 parameters['properties'][prop_key] = prop_value
+                if prop_key in ori_required:
+                    required.append(prop_key)
+
             elif prop_value['type'] == 'array':
-                parameters['properties'][prop_key] = prop_value
+                # escape the nested object in array
+                # todo: flattern the list[object] to list[k1], list[k2]
+                if prop_value['items']['type'] in ['string', 'number', 'boolean', 'integer', 'int', 'float']:
+                    parameters['properties'][prop_key] = prop_value
+                    if prop_key in ori_required:
+                        required.append(prop_key)
+
             elif prop_value['type'] == 'object':
                 # flatten the nested object for the command-r func call
                 # not support the nested nested object parameter
+                # Special case: e.g. {'type': 'object', 'description': 'The graph represented as an adjacency matrix'}
+                if 'properties' not in prop_value and 'description' in prop_value:
+                    parameters['properties'][prop_key] = prop_value
+                    if prop_key in ori_required:
+                        required.append(prop_key)
+                    continue
+                # Normal case: e.g. {'type': 'object', 'properties': {'a': {'type': 'string', 'description': 'desc'}}}    
                 for sub_prop_key, sub_prop_value in prop_value['properties'].items():
                     if sub_prop_value['type'] == 'object':
                         print('ERROR: not support the nested nested object parameter')
                         continue
 
                     parameters['properties'][sub_prop_key] = sub_prop_value
+                    if sub_prop_key in prop_value.get('required', []):
+                        required.append(sub_prop_key)
             else:
                 pass
+        
+        if required:
+            parameters['required'] = required
 
         tool_def = {"type": "function", "function": {"name": name, "description": desc, "parameters": parameters}}
         tools.append(tool_def)
 
+    if not tools:
+        return []
+    
     request = {}
     request['tools'] = tools
     request['messages'] = []
@@ -69,20 +93,28 @@ def trans_conv_oai_request(conv):
 
     return requests
 
-def perf(test_data, api_url, model_name, num_parallel):
+def perf(test_data, api_url, model_name, num_parallel, cnt):
+    conversations = json.load(open(test_data))[1000:1000+cnt]
+    bodys = []
+    for coversation in conversations:
+        bodys.extend(trans_conv_oai_request(coversation))
+    
+    n = len(bodys)
+    print('Total requests: {}'.format(n))
     if num_parallel == 1:
-        conversations = json.load(open(test_data))
-        for coversation in conversations[50:100]:
-            bodys = trans_conv_oai_request(coversation)
-            batch_call(bodys, api_url, model_name)
+        bodys = bodys
+        result_file = f'./outputs/result_single_{n}.txt'
+        batch_call(bodys, api_url, model_name, result_file)
     else:
-        n = len(conversations)
         batch_size = (n + 1) // num_parallel
+        if batch_size == 0:
+            batch_size = 1
+
         threads = []
         for i in range(0, n, batch_size):
-            threads.append(
-                threading.Thread(target=trans_conv_oai_request, args=((conversations[i: i + batch_size],)))
-            )
+            result_file = './outputs/result_batch_{0}_{1}.txt'.format(n, i)
+            batch_bodys = bodys[i: i + batch_size]
+            threads.append(threading.Thread(target=batch_call, args=(batch_bodys, api_url, model_name, result_file)))
         
         for thread in threads:
             thread.start()
@@ -96,9 +128,11 @@ def main():
     parser.add_argument('-u', '--api-url', type=str, required=False, default='http://127.0.0.1:9100/v1', help='api url')
     parser.add_argument('-d', '--test-data', type=str, required=False, default='glaive_toolcall_10k.json', help='test data')
     parser.add_argument('-n', '--num-parallel', type=int, required=False, default=1, help="num parallel")
+    parser.add_argument('-c', '--cnt', type=int, required=False, default=1000, help="run count")
+    
     
     args = parser.parse_args()
-    perf(args.test_data, args.api_url, args.model_name, args.num_parallel)
+    perf(args.test_data, args.api_url, args.model_name, args.num_parallel, args.cnt)
 
 if __name__ == '__main__':
    main()
--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -2,29 +2,28 @@
 
 
 function run_qwen_32b() {
-    export CUDA_VISIBLE_DEVICES=1
+    export CUDA_VISIBLE_DEVICES=2
     model="/mnt/disks/elem-disk1/workspace/public/llm/Qwen1.5-32B-Chat-GGUF/qwen1_5-32b-chat-q5_k_m.gguf"
-    ./build/bin/server -m $model -c 30000 -n 2024 --host 0.0.0.0 --port 9301 --n-gpu-layers 100
+    ./build/bin/server -m $model -c 30000 -n 2024 --host 0.0.0.0 --port 9301 --n-gpu-layers 100 -cb -ub 32 -np 8
 }
 
 function run_qwen_72b() {
     export CUDA_VISIBLE_DEVICES=0,1
     model="/mnt/disks/elem-disk1/workspace/public/llm/Qwen1.5-72B-Chat-GGUF/qwen1_5-72b-chat-q5_k_m.gguf"
-    ./build/bin/server -m $model -c 30000 -n 2024 --host 0.0.0.0 --port 9301 --n-gpu-layers 100
+    ./build/bin/server -m $model -c 30000 -n 2024 --host 0.0.0.0 --port 9300 --n-gpu-layers 100 -cb -ub 32 -np 8
 }
 
 function run_cohere() {
     export CUDA_VISIBLE_DEVICES=2
     model="/opt/bisheng-rt/models/model_repository/command-r-gguf/c4ai-command-r-v01-Q5_K_M.gguf"
-   ./build/bin/server -m $model -c 60000 -n 2048 --host 0.0.0.0 --port 9100 --n-gpu-layers 100 --chat-template command-r
+   ./build/bin/server -m $model -c 60000 -n 2048 --host 0.0.0.0 --port 9100 --n-gpu-layers 100 --chat-template command-r -cb -ub 32 -np 8
 }
 
 function run_command_r_plus() {
     export CUDA_VISIBLE_DEVICES=2,3
     model="/mnt/disks/elem-disk1/workspace/public/llm/c4ai-command-r-plus-iMat.GGUF/ggml-c4ai-command-r-plus-104b-q5_k_s-00001-of-00002.gguf"
-   ./build/bin/server -m $model -c 100000 -n 2048 --host 0.0.0.0 --port 9100 --n-gpu-layers 200 --chat-template command-r
+   ./build/bin/server -m $model -c 100000 -n 2048 --host 0.0.0.0 --port 9100 --n-gpu-layers 200 --chat-template command-r -cb -ub 32 -np 8
 }
-
 
 # run_qwen_72b
 # run_qwen_32b
